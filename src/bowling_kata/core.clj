d0da9b1c6d95b1faf41f55a2087af01bd8816734
Floyd May <floyd-may@github>
2015-01-19 22:05:51 -0600

    WIP for second attempt

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,3 +1,7 @@
 (ns bowling-kata.core
   (:gen-class))
 
+(defrecord Frame [first-roll second-roll])
+
+(defn roll [state pins]
+  nil)
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,3 +2,8 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all :as core]))
 
+(describe "game state management"
+  (it "should create a frame"
+    (should= 
+              [(core/->Frame 0)]
+              (core/roll [] 0))))
