c54287abafbe4dec0ddbca70dc88b765c1771159
Floyd May <floyd-may@github>
2015-02-01 09:55:37 -0600

    progress towards smarter game

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,3 +1,16 @@
 (ns bowling-kata.core
   (:gen-class))
 
+(defrecord OpenFrame [roll])
+
+(defprotocol Game
+  (frames [this])
+  (score [this]))
+
+
+(defn roll [roll]
+  (reify Game
+    (frames [_] [(->OpenFrame roll)])
+    (score [_] 0)))
+
+; let's remember dispatch on type
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,3 +2,12 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all :as core]))
 
+(describe "gutter game"
+  (describe "first roll"
+    (with game (core/roll 0))
+    (it "should create an open frame"
+        (should= [(core/->OpenFrame 0)]
+                 (frames @game)))
+    (it "should score zero"
+        (should= 0 (score @game)))))
+               
