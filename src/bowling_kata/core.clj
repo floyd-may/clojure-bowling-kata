2b020c5e22ed00a4242fdd68ea891d497a481685
Floyd May <floyd-may@github>
2015-02-01 08:37:26 -0600

    down the path of just tracking the rolls without scoring

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,7 +1,56 @@
 (ns bowling-kata.core
   (:gen-class))
 
-(defrecord Frame [first-roll second-roll])
+(defprotocol Frame
+  (is-closed [this]))
 
-(defn roll [state pins]
-  nil)
+(defrecord Game [frames add-roll-fn])
+
+(defrecord OpenFrame [roll]
+  Frame
+  (is-closed [this] false))
+
+(defrecord ClosedFrame [first-roll second-roll]
+  Frame
+  (is-closed [this] true))
+
+(defrecord SpareFrame [first-roll second-roll]
+  Frame
+  (is-closed [this] true))
+
+(defrecord StrikeFrame []
+  Frame
+  (is-closed [this] true))
+
+(defrecord FinalFrame [first-roll second-roll third-roll]
+  Frame
+  (is-closed [this] true))
+
+(declare -add-frame)
+
+(defn create-closed-frame [first-roll second-roll]
+  (if (= 10 (+ first-roll second-roll))
+      (->SpareFrame first-roll second-roll)
+      (->ClosedFrame first-roll second-roll)))
+
+(defn create-new-frame [roll]
+  (if (= 10 roll)
+      (->StrikeFrame)
+      (->OpenFrame roll)))
+
+(defn -close-frame [frames first-roll second-roll]
+  (let [new-frames (conj frames (create-closed-frame first-roll second-roll))]
+    (->Game new-frames
+            (partial -add-frame new-frames))))
+
+(defn -add-frame [frames roll]
+  (let [new-frame (create-new-frame roll)
+        new-frames (conj frames new-frame)]
+    (->Game new-frames
+            (if (is-closed new-frame)
+                (partial -add-frame new-frames)
+                (partial -close-frame frames roll)))))
+
+(defn new-game [first-roll]
+  (-add-frame [] first-roll))
+
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,8 +2,50 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all :as core]))
 
-(describe "game state management"
-  (it "should create a frame"
-    (should= 
-              [(core/->Frame 0)]
-              (core/roll [] 0))))
+(describe "new-game function"
+  (it "should create a new game with an open frame"
+      (should= 
+              [(core/->OpenFrame 0)]
+              (:frames (core/new-game 0))))
+  (it "should create a new game with a strike frame"
+      (should= 
+              [(core/->StrikeFrame)]
+              (:frames (core/new-game 10)))))
+
+
+(defn roll-game [rolls]
+  (let [[first-roll & remaining-rolls] rolls
+        initial-state (core/new-game first-roll)
+        reduce-fn #((:add-roll-fn %1) %2)]
+    (reduce reduce-fn initial-state remaining-rolls)))
+
+(describe "game add-roll-fn"
+  (it "should create frame from open frame"
+    (should= [(core/->ClosedFrame 0 0)]
+             (:frames (roll-game [0 0]))))
+  (it "should create open frame after initial strike"
+    (should= [(core/->StrikeFrame) (core/->OpenFrame 0)]
+             (:frames (roll-game [10 0]))))
+  (it "should create new open frame after closed frame"
+    (should= [(core/->ClosedFrame 0 0) (core/->OpenFrame 0)]
+             (:frames (roll-game [0 0 0]))))
+  (it "should create 10 frames of gutterballs"
+    (should= (vec (take 10 (repeat (core/->ClosedFrame 0 0))))
+             (:frames (roll-game (take 20 (repeat 0))))))
+  (it "should insert spare frame"
+    (should= [
+                (core/->ClosedFrame 0 0)
+                (core/->ClosedFrame 0 0)
+                (core/->ClosedFrame 0 0)
+                (core/->SpareFrame 3 7)
+             ]
+             (:frames (roll-game [0 0 0 0 0 0 3 7]))))
+  (it "should insert strike frame"
+    (should= [
+                (core/->ClosedFrame 0 0)
+                (core/->ClosedFrame 0 0)
+                (core/->StrikeFrame)
+                (core/->ClosedFrame 0 0)
+             ]
+             (:frames (roll-game [0 0 0 0 10 0 0])))))
+  
