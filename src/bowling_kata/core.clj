4565f40ed6aedab1cc86a45433ffe8d9b8b1ebd0
Floyd May <floyd-may@github>
2015-02-01 09:37:52 -0600

    my oo past still haunts me

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,3 +1,30 @@
 (ns bowling-kata.core
   (:gen-class))
 
+(defrecord ClosedFrame [first-roll second-roll])
+(defrecord OpenFrame [roll])
+
+(defprotocol Game
+  (frames [this])
+  (add-roll [this roll]))
+
+(declare append-roll-to-open-frame)
+
+(defn- append-frame-to-game [game roll]
+  (reify Game
+    (frames [this] (conj (frames game) (->OpenFrame roll)))
+    (add-roll [_ next-roll] #(append-roll-to-open-frame game roll next-roll))))
+
+(defn- append-roll-to-open-frame [game first-roll second-roll]
+  (let [initial-frames (frames game)
+        new-frame (->ClosedFrame first-roll second-roll)
+        all-frames (conj initial-frames new-frame)]
+    (reify Game
+      (frames [this] all-frames)
+      (add-roll [_ _] (+ 0 0)))))
+
+(defn new-game []
+  (reify Game
+    (frames [this] [])
+    (add-roll [this roll] #(append-frame-to-game this roll))))
+
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,3 +2,18 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all :as core]))
 
+(defn roll-game [rolls]
+  (let [initial-state (core/new-game)
+        reduce-fn #(add-roll %1 %2)]
+    (reduce reduce-fn initial-state rolls)))
+  
+(describe "gutter game"
+  (it "should represent first roll as an open frame"
+    (should= [(core/->OpenFrame 0)]
+             (frames ((add-roll (core/new-game) 0)))))
+  (it "should represent two rolls as a closed frame"
+    (should= [(core/->ClosedFrame 0 0)]
+             (frames (roll-game [0 0])))))
+  ;(it "should be represented as ten frames of two gutterballs"
+  ;  (should= (vec (take 10 (repeat (core/->ClosedFrame 0 0))))
+  ;           (frames (reduce #(add-roll %1 %2) (core/new-game) (take 20 (repeat 0)))))))
