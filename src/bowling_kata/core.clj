061fd9e63f2dd55970a814efdf9ab3c824a8870f
Floyd May <floyd-may@github>
2015-02-01 22:58:42 -0600

    beginnings of using dispatch on record type

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -2,15 +2,32 @@
   (:gen-class))
 
 (defrecord OpenFrame [roll])
+(defrecord ClosedFrame [first-roll second-roll])
+(defrecord EmptyFrame [])
 
 (defprotocol Game
-  (frames [this])
+  (closed-frames [this])
+  (open-frame [this])
   (score [this]))
 
-
-(defn roll [roll]
+(defmulti append-roll (fn [game pins] (type (open-frame game))))
+(defmethod append-roll EmptyFrame [game pins]
   (reify Game
-    (frames [_] [(->OpenFrame roll)])
+    (closed-frames [_] (closed-frames game))
+    (open-frame [_] (->OpenFrame pins))
+    (score [_] 0)))
+(defmethod append-roll OpenFrame [game pins]
+  (let [first-roll (:roll (open-frame game))
+        frames (closed-frames game)]
+    (reify Game
+      (closed-frames [_] (conj frames (->ClosedFrame first-roll pins)))
+      (open-frame [_] (->EmptyFrame))
+      (score [_] 0))))
+
+(def empty-game 
+  (reify Game
+    (closed-frames [_] [])
+    (open-frame [_] (->EmptyFrame))
     (score [_] 0)))
 
-; let's remember dispatch on type
+
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -4,10 +4,27 @@
 
 (describe "gutter game"
   (describe "first roll"
-    (with game (core/roll 0))
+    (with game (core/append-roll core/empty-game 0))
     (it "should create an open frame"
-        (should= [(core/->OpenFrame 0)]
-                 (frames @game)))
+        (should= (core/->OpenFrame 0)
+                 (open-frame @game)))
+    (it "should score zero"
+        (should= 0 (score @game))))
+  (describe "second roll"
+    (with game (reduce core/append-roll core/empty-game [0 0]))
+    (it "should create a closed frame"
+        (should= [(core/->ClosedFrame 0 0)]
+                 (closed-frames @game)))
     (it "should score zero"
         (should= 0 (score @game)))))
+  ;(describe "third roll"
+  ;  (with game (reduce core/roll [0 0]))
+  ;  (it "should create a closed frame and an open frame"
+  ;      (should= [(core/->ClosedFrame 0 0)]
+  ;               (closed-frames @game))
+  ;      (should= (core/->OpenFrame 0)
+  ;               (open-frame @game)))
+  ;  (it "should score zero"
+  ;      (should= 0 (score @game)))))
+    
                
