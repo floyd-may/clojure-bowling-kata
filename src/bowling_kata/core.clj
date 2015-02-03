efab67414ed5740edf15ceeb284fbc711d90987f
Floyd May <floyd-may@github>
2015-02-02 22:39:57 -0600

    add handling for strikes

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,33 +1,58 @@
 (ns bowling-kata.core
   (:gen-class))
 
-(defrecord OpenFrame [roll])
-(defrecord ClosedFrame [first-roll second-roll])
-(defrecord EmptyFrame [])
+(defprotocol Frame
+  (rolls [this]))
 
-(defprotocol Game
-  (closed-frames [this])
-  (open-frame [this])
-  (score [this]))
+(defrecord OpenFrame [roll]
+  Frame
+  (rolls [this] [roll]))
+(defrecord ClosedFrame [first-roll second-roll]
+  Frame
+  (rolls [this] [first-roll second-roll]))
+(defrecord EmptyFrame []
+  Frame
+  (rolls [this] []))
+(defrecord StrikeFrame []
+  Frame
+  (rolls [this] [10]))
 
-(defmulti append-roll (fn [game pins] (type (open-frame game))))
+(defrecord Game [closed-frames open-frame])
+
+(defmulti append-roll (fn [game pins] (type (:open-frame game))))
 (defmethod append-roll EmptyFrame [game pins]
-  (reify Game
-    (closed-frames [_] (closed-frames game))
-    (open-frame [_] (->OpenFrame pins))
-    (score [_] 0)))
+  (if (= 10 pins)
+      (->Game (conj (:closed-frames game) (->StrikeFrame))
+              (->EmptyFrame))
+      (->Game (:closed-frames game) (->OpenFrame pins))))
 (defmethod append-roll OpenFrame [game pins]
-  (let [first-roll (:roll (open-frame game))
-        frames (closed-frames game)]
-    (reify Game
-      (closed-frames [_] (conj frames (->ClosedFrame first-roll pins)))
-      (open-frame [_] (->EmptyFrame))
-      (score [_] 0))))
+  (let [first-roll (:roll (:open-frame game))
+        frames (:closed-frames game)]
+    (->Game (conj frames (->ClosedFrame first-roll pins)) (->EmptyFrame))))
 
-(def empty-game 
-  (reify Game
-    (closed-frames [_] [])
-    (open-frame [_] (->EmptyFrame))
-    (score [_] 0)))
+(def empty-game (->Game [] (->EmptyFrame)))
 
+(defmulti frame-score (fn [frame later-frames] (type frame)))
+(defmethod frame-score ClosedFrame [frame later-frames]
+  (+ (:first-roll frame) (:second-roll frame)))
+(defmethod frame-score StrikeFrame [frame later-frames]
+  (let [later-rolls (flatten (map rolls later-frames))]
+    (+ 10 (reduce + (take 2 later-rolls)))))
+
+(defn- -score [tmp-score game]
+  (let [frames (:closed-frames game)]
+    (if
+      (seq frames)
+      (let [[frame & later-frames] frames]
+        (recur
+          (+ tmp-score (frame-score frame later-frames))
+          (->Game later-frames (:open-frame game))))
+      tmp-score)))
+          
+
+(defn score [game]
+  (-score 0 game))
+
+(defn roll-game [rolls]
+  (reduce append-roll empty-game rolls))
 
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -6,25 +6,53 @@
   (describe "first roll"
     (with game (core/append-roll core/empty-game 0))
     (it "should create an open frame"
+        (should= []
+                 (:closed-frames @game))
         (should= (core/->OpenFrame 0)
-                 (open-frame @game)))
+                 (:open-frame @game)))
     (it "should score zero"
         (should= 0 (score @game))))
   (describe "second roll"
     (with game (reduce core/append-roll core/empty-game [0 0]))
     (it "should create a closed frame"
         (should= [(core/->ClosedFrame 0 0)]
-                 (closed-frames @game)))
+                 (:closed-frames @game))
+        (should= (core/->EmptyFrame)
+                 (:open-frame @game)))
+    (it "should score zero"
+        (should= 0 (score @game))))
+  (describe "third roll"
+    (with game (core/roll-game [0 0 0]))
+    (it "should create a closed frame and an open frame"
+        (should= [(core/->ClosedFrame 0 0)]
+                 (:closed-frames @game))
+        (should= (core/->OpenFrame 0)
+                 (:open-frame @game)))
+    (it "should score zero"
+        (should= 0 (score @game))))
+  (describe "complete game"
+    (with game (core/roll-game (repeat 20 0)))
+    (it "should create a closed frame and an open frame"
+        (should= (vec (repeat 10 (core/->ClosedFrame 0 0)))
+                 (:closed-frames @game))
+        (should= (core/->EmptyFrame)
+                 (:open-frame @game)))
     (it "should score zero"
         (should= 0 (score @game)))))
-  ;(describe "third roll"
-  ;  (with game (reduce core/roll [0 0]))
-  ;  (it "should create a closed frame and an open frame"
-  ;      (should= [(core/->ClosedFrame 0 0)]
-  ;               (closed-frames @game))
-  ;      (should= (core/->OpenFrame 0)
-  ;               (open-frame @game)))
-  ;  (it "should score zero"
-  ;      (should= 0 (score @game)))))
-    
-               
+
+(describe "all ones"
+  (with game (core/roll-game (repeat 20 1)))
+  (it "should have ten closed frames"
+    (should= (vec (repeat 10 (core/->ClosedFrame 1 1)))
+             (:closed-frames @game)))
+  (it "should score 20"
+    (should= 20 (score @game))))
+
+(describe "leading strike"
+  (with game (core/roll-game [10 3 4]))
+  (it "should have two closed frames"
+    (should=
+          [(core/->StrikeFrame) (core/->ClosedFrame 3 4)]
+          (:closed-frames @game)))
+  (it "should score 24"
+    (should= 24 (score @game))))
